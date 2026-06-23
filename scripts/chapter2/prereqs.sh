e6ecc269642a7df4127a2261fb92a82a6196dd42
voncloft <voncloft@gmail.com>
2026-06-23 18:40:17 -0400

    Updates

--- scripts/chapter2/prereqs.sh
+++ scripts/chapter2/prereqs.sh
@@ -1,4 +1,9 @@
 set -euo pipefail
 echo "step:Installing prerequisites of host os"
 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y binutils bison gawk gcc g++ m4 make texinfo yacc bash </dev/null
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y \
+  -o Dpkg::Use-Pty=0 \
+  -o Dpkg::Progress-Fancy=0 \
+  -o APT::Color=0 \
+  install binutils bison gawk gcc g++ m4 make texinfo yacc bash </dev/null
