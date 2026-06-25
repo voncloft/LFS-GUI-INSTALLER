a5f140455b0f41c05f486bafce84e02ad63d7a88
voncloft <voncloft@gmail.com>
2026-06-25 07:36:51 -0400

    Updates

--- scripts/chapter3/get_packages.sh
+++ scripts/chapter3/get_packages.sh
@@ -15,7 +15,7 @@ chmod -v a+wt $LFS/sources
 
 
 wget -c https://ftp.osuosl.org/pub/lfs/lfs-packages/lfs-packages-13.0-rc1.tar --directory-prefix=$LFS/sources
-tar -xvf $LFS/sources/lfs-packages-13.0-rc1.tar
+tar -xvf $LFS/sources/lfs-packages-13.0-rc1.tar -C $LFS/sources
 
 cp ../../tools/autountar.sh $LFS/sources/autountar
 chmod +x $LFS/sources/autountar
