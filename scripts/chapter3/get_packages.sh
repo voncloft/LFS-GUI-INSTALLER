160c7b766b9f6bb372cb260be205147a255d5594
voncloft <voncloft@gmail.com>
2026-06-25 07:34:29 -0400

    Updates

--- scripts/chapter3/get_packages.sh
+++ scripts/chapter3/get_packages.sh
@@ -10,16 +10,22 @@ chmod -v a+wt $LFS/sources
 #pushd $LFS/sources
 #  md5sum -c md5sums
 #popd
-wget https://sourceware.org/pub/binutils/releases/binutils-2.46.0.tar.xz
-mv binutils*.tar.xz $LFS/sources
+#wget https://sourceware.org/pub/binutils/releases/binutils-2.46.0.tar.xz
+#mv binutils*.tar.xz $LFS/sources
+
+
+wget -c https://ftp.osuosl.org/pub/lfs/lfs-packages/lfs-packages-13.0-rc1.tar --directory-prefix=$LFS/sources
+tar -xvf $LFS/sources/lfs-packages-13.0-rc1.tar
+
 cp ../../tools/autountar.sh $LFS/sources/autountar
 chmod +x $LFS/sources/autountar
 
 git clone https://github.com/voncloft/scratchpkg.git
 mv scratchpkg $LFS/sources
 rm $LFS/sources/scratchpkg/scratchpkg.repo
-echo "/sources" > $LFS/sources/scratchpkg/scratchpkg.repo
-echo "SOURCE_DIR=/sources" >> $LFS/sources/scratchpkg/scratchpkg.conf
+echo "#lfs" >> $LFS/sources/scratchpkg/scratchpkg.repo
+echo "/sources" >> $LFS/sources/scratchpkg/scratchpkg.repo
+#echo "SOURCE_DIR=/sources" >> $LFS/sources/scratchpkg/scratchpkg.conf
 tar cJf $LFS/sources/scratchpkg.tar.xz $LFS/sources/scratchpkg
 
 rm -rfv $LFS/sources/scratchpkg
